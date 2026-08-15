8ddc814d59d467de6a46611d11aae0801e20c240
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-08-14 23:06:59 -0400

    chore: SSR removal

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -10,8 +9,7 @@ export default defineConfig({
 			compilerOptions: {
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter()
+			}
 		})
 	]
 });
